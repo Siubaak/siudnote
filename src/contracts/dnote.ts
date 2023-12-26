044785eb8ce18d9e697118437116dd450dcc1b5f
Siubaak <jimkwan.yau@gmail.com>
2023-12-26 19:55:38 +0800

    update

--- src/contracts/dnote.ts
+++ src/contracts/dnote.ts
@@ -54,7 +54,7 @@ const abi = [
 ]
 
 export default {
-  address: '0x5B95C980607A52e5E3Cc772BB25a3865D8BD0374' as `0x${string}`, // sepolia
+  address: '0x3193F31Fda5E25ca567e2E9d2C8DfebCB0cb0395' as `0x${string}`, // sepolia
   // address: '0x5FbDB2315678afecb367f032d93F642f64180aa3' as `0x${string}`,
   abi,
 }
