1a9c8178f7ab4ce4eaba372d32ec5a9506f4bee7
siubaak <q.yk@qq.com>
2025-01-23 19:50:23 +0800

    Upgrade React and related dependencies to version 19.0.0; refactor App component to include ThemeProvider for consistent theming across the application.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import { NoteCarousel } from '@/components/NoteCarousel'
 import { NoteInput } from '@/components/NoteInput'
 import { MagnetLines } from '@/components/ui/magnet-lines'
 
-import { ThemeProvider } from '@/components/ThemeProvider'
-
 export default function App() {
   const { isConnected } = useAccount()
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <>
       <MagnetLines className="absolute inset-0 z-[-1]" lineColor="hsla(var(--border))" containerSize="full"></MagnetLines>
       <div className="flex flex-col h-screen">
         <div className={'flex items-center px-3 sm:px-6 py-3' + (isConnected ? ' justify-between' : ' justify-center h-screen flex-col')}>
@@ -26,6 +24,6 @@ export default function App() {
           </>
         )}
       </div>
-    </ThemeProvider>
+    </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 import App from './App.tsx'
 import { config } from './wagmi.ts'
@@ -17,7 +18,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <App />
+        </ThemeProvider>
       </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>,
